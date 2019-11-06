ded8c7808824e6eef50f4c743516fa9b4fab3e27
Julio <julioj.lopeza@gmail.com>
2019-11-05 22:21:04 -0300

    arrow functions

---
+++ 02-fundamentos/arrowFunctions.js
@@ -0,0 +1,13 @@
+function suma(a,b){
+    return a+b
+}
+
+console.log(suma(10,20))
+
+let sumar = (a,b) => a+b
+
+console.log(sumar(10,20))
+
+let saludar = () => "hola mundo"
+
+console.log(saludar())
